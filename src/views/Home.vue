c61227c8d267a8bb4dca45c6c4d3f856e1c664b0
Yuriy Bespechanskiy <eckhart1967@gmail.com>
2024-12-10 14:49:35 +0300

    Change Home.vue 5

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -125,7 +125,7 @@ const handleEntranceForAll = () => {
 }
 
 .title {
-  align-self: center;
+  text-align: center;
   font-size: 2.5rem;
   margin-bottom: 2rem;
   text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5);
